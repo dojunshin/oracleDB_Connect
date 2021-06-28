a32177a82b85e3012b78c0beb7aba5239325a8a7
PNUH_DOJUNSHIN <dojun2029@hanmail.net>
2021-06-28 13:56:12 +0900

    깃사용법 적어놈..

---
+++ app/src/main/java/com/example/oracledb_connect/submain.java
@@ -0,0 +1,12 @@
+package com.example.oracledb_connect;
+
+public class submain {
+    //Git 파일 수정 관련
+    /**
+     *  붉은색 : Git에 추가되지 않은 신규파일 (Add대상)
+     *  초록색 : Git에 Add된 파일(Commit & Push 대상)
+     *  파란색 : Git에 추가 된 이후 수정이 발생한 파일 (Commit & Push 대상)
+     */
+
+
+}
